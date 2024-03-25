2fdb6f0390f3ee239ea48ab9490bd3dbf46aa631
Alen Alempijevic <Alen.Alempijevic@uts.edu.au>
2024-03-25 16:59:05 +1100

    First attempt to include the race track

---
+++ pfms-ros/gazebo_tf/launch/audi_track.launch.py
@@ -0,0 +1,102 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, PushRosNamespace
+from ament_index_python.packages import get_package_share_directory
+
+ARGUMENTS = [
+    DeclareLaunchArgument('world_path', default_value=PathJoinSubstitution(
+        [FindPackageShare("aws_robomaker_racetrack_world"), "worlds", "racetrack_day.world"]),
+        description='The world path, by default is racetrack_day.world'),
+    DeclareLaunchArgument('gui', default_value='true',
+                          description='Whether to launch the GUI'),
+]
+
+
+def generate_launch_description():
+
+    # Launch args
+    world_path = LaunchConfiguration('world_path')
+    # prefix = LaunchConfiguration('prefix')
+
+  
+    # Gazebo server
+    gzserver = ExecuteProcess(
+        cmd=['gzserver',
+             '-s', 'libgazebo_ros_init.so',
+             '-s', 'libgazebo_ros_factory.so',
+             world_path],
+        output='screen',
+    )
+
+    # Gazebo client
+    gzclient = ExecuteProcess(
+        cmd=['gzclient'],
+        output='screen',
+        condition=IfCondition(LaunchConfiguration('gui')),
+    )
+
+    gazebo_connect = Node(
+        package='gazebo_tf',
+        executable='gazebo_connect',
+        name='gazebo_connect',
+        parameters=[{'use_sim_time': False}]
+        # arguments=['-d', os.path.join(get_package_share_directory('audibot_gazebo'), 'rviz', 'two_vehicle_example.rviz')]
+    )
+
+    orange_audibot_options = dict(
+        robot_name = 'orange',
+        start_x = '8.5',
+        start_y = '15',
+        start_z = '0',
+        start_yaw = '0',
+        pub_tf = 'true',
+        tf_freq = '100.0',
+        blue = 'false'
+    )
+    spawn_orange_audibot = GroupAction(
+        actions=[
+            PushRosNamespace('orange'),
+             IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([
+                    os.path.join(get_package_share_directory('audibot_gazebo'), 'launch', 'audibot_robot.launch.py')
+                ]),
+                launch_arguments=orange_audibot_options.items()
+            )
+        ]
+    )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='two_vehicle_viz',
+        # output='screen',
+        output={'both': 'log'},
+        arguments=['-d', os.path.join(get_package_share_directory('gazebo_tf'), 'rviz', 'audi_husky.rviz')]
+    )
+
+    audi_reach = Node(
+        package='gazebo_tf',
+        executable='reach',
+        name='audi_reach',
+        output='screen'
+        # output={'both': 'log'},
+        # arguments=['-d', os.path.join(get_package_share_directory('gazebo_tf'), 'rviz', 'audi_husky.rviz')]
+    )
+
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(gzserver)
+    ld.add_action(gzclient)
+    ld.add_action(gazebo_connect)
+    ld.add_action(spawn_orange_audibot)
+    ld.add_action(rviz)
+    ld.add_action(audi_reach)
+
+    return ld

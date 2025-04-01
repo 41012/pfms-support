f9b890c05a86d004abfe324411fc62093cac9558
Alen Alempijevic <Alen.Alempijevic@uts.edu.au>
2025-04-01 22:04:23 +1100

    initially fixed to supply starting position

--- pfms-ros/sjtu_drone/sjtu_drone_bringup/sjtu_drone_bringup/spawn_drone.py
+++ pfms-ros/sjtu_drone/sjtu_drone_bringup/sjtu_drone_bringup/spawn_drone.py
@@ -17,6 +17,9 @@
 import sys
 import rclpy
 from gazebo_msgs.srv import SpawnEntity
+from geometry_msgs.msg import Pose
+#from tf_transformations import quaternion_from_euler  # Import for quaternion conversion
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -26,11 +29,39 @@ def main(args=None):
     content = sys.argv[1]
     namespace = sys.argv[2]
 
+    # Get the initial position
+    x = 0.0
+    y = 0.0
+    yaw =0.0
+    if len(sys.argv) > 2:
+        x = float(sys.argv[3])
+    if len(sys.argv) > 3:
+        y = float(sys.argv[4])
+    # Get the initial orientation
+    if len(sys.argv) > 43:
+        yaw = float(sys.argv[5])
+
     req = SpawnEntity.Request()
     req.name = namespace
     req.xml = content
     req.robot_namespace = namespace
     req.reference_frame = "world"
+
+    # Set the initial pose
+    initial_pose = Pose()
+    initial_pose.position.x = x
+    initial_pose.position.y = y
+    initial_pose.position.z = 0.0
+    
+    # Convert yaw to quaternion
+    # quaternion = quaternion_from_euler(0.0, 0.0, yaw)  # Roll and pitch are 0.0
+    # initial_pose.orientation.x = quaternion[0]
+    # initial_pose.orientation.y = quaternion[1]
+    # initial_pose.orientation.z = quaternion[2]
+    # initial_pose.orientation.w = quaternion[3]
+    initial_pose.orientation.w = 1.0
+
+    req.initial_pose = initial_pose    
 
     while not cli.wait_for_service(timeout_sec=1.0):
         node.get_logger().info('service not available, waiting again...')
